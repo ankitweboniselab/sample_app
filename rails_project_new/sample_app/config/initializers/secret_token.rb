9d652ad76e03ee8f66951e1427ad7d6ae68aad75
ankit <ankit@weboniselab.com>
2011-07-27 12:42:15 +0530

    full application codes

---
+++ rails_project_new/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '93ce23315a1e55dc3453bbf3331f56de79c705835ae0ce874bed2cc78737b1601553a6b5746569a15d79bcb815a0e4f427918ae9ad7aaa32dcf5375999f9a875'
